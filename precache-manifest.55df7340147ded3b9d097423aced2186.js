91547a916bef4c54995da8e89d689fb76c53721f
Aleksandar Radulovic <aleksandar_radulovic@rocketmail.com>
2019-08-04 19:31:50 +0200

    Updates

---
+++ precache-manifest.55df7340147ded3b9d097423aced2186.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f108c71f7e3656a0a111015a5cd0afa",
+    "url": "/alexgooner12/expenser-gth-pages/index.html"
+  },
+  {
+    "revision": "bb5f62af8f71f39bf1fe",
+    "url": "/alexgooner12/expenser-gth-pages/static/css/main.e15eab71.chunk.css"
+  },
+  {
+    "revision": "71fcb20ce3edca5cbb62",
+    "url": "/alexgooner12/expenser-gth-pages/static/js/2.cb41f16e.chunk.js"
+  },
+  {
+    "revision": "bb5f62af8f71f39bf1fe",
+    "url": "/alexgooner12/expenser-gth-pages/static/js/main.977955ba.chunk.js"
+  },
+  {
+    "revision": "3444fd3ac663384f854f",
+    "url": "/alexgooner12/expenser-gth-pages/static/js/runtime~main.fa9f405a.js"
+  }
+]);
